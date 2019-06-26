097f4d62b618dd806494344b2179c748ae77b05f
frajerzycki <frajerzycki@protonmail.com>
2019-06-26 15:48:46 +0200

    Sending messages between clients works.

--- server.c
+++ server.c
@@ -13,8 +13,8 @@
 #define SERVER_ARGUMENTS_TEMPLATE "<PORT>"
 
 int main(int argc, char *argv[]) {
-  unsigned int i, max_clients = 100;
-  int master_socket, addrlen, new_socket, client_socket[30], activity, valread, opt = 1, max_sd, port, sdi;
+  unsigned int i, max_clients = 30;
+  int master_socket, addrlen, new_socket, client_socket[max_clients], activity, valread, opt = 1, max_sd, port, sdi;
   struct sockaddr_in address;
 
   char buffer[BUFFER_SIZE + 1]; // data buffer of 1K
@@ -31,36 +31,25 @@ int main(int argc, char *argv[]) {
     client_socket[i] = 0;
 
   // create a master socket
-  if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
+  if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    show_error_message_and_exit();
 
   // set master socket to allow multiple connections
   if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
-                 sizeof(opt)) < 0) {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
-  }
-
+                 sizeof(opt)) < 0)
+    show_error_message_and_exit();
   // type of socket created
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(port);
 
   // bind the socket to localhost port 8888
-  if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
-  printf("Listener on port %d \n", port);
+  if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0) 
+    show_error_message_and_exit();
 
   // try to specify maximum of 3 pending connections for the master socket
-  if (listen(master_socket, 3) < 0) {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
-
+  if (listen(master_socket, 3) < 0) 
+    show_error_message_and_exit();
   // accept the incoming connection
   addrlen = sizeof(address);
   puts("Waiting for connections ...");
@@ -98,20 +87,16 @@ int main(int argc, char *argv[]) {
     // then its an incoming connection
     if (FD_ISSET(master_socket, &readfds)) {
       if ((new_socket = accept(master_socket, (struct sockaddr *)&address,
-                               (socklen_t *)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-      }
+                               (socklen_t *)&addrlen)) < 0) 
+        show_error_message_and_exit();
 
       // inform user of socket number - used in send and receive commands
-      printf("New connection, socket fd is %d, ip is : %s, port : %d\n",
-             new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+      printf("New connection from %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
       for (i = 0; i < max_clients; i++) {   
         //if position is empty  
         if( client_socket[i] == 0) {   
             client_socket[i] = new_socket;   
-            printf("Adding to list of sockets as %d\n" , i);   
             break;   
         }   
       }   
@@ -127,7 +112,7 @@ int main(int argc, char *argv[]) {
         if ((valread = read(sdi, buffer, BUFFER_SIZE)) == 0) {
           // Somebody disconnected , get his details and print
           getpeername(sdi, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-          printf("Host disconnected, ip %s, port %d\n",
+          printf("Host disconnected %s:%d\n",
                  inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
           // Close the socket and mark as 0 in list for reuse
@@ -137,7 +122,7 @@ int main(int argc, char *argv[]) {
           buffer[valread] = 0;
           for (unsigned int j = 0; j < max_clients; j++) {
             int sdj = client_socket[j];
-            if (i != j && FD_ISSET(sdi, &readfds))
+            if (sdj > 0 && i != j)
               send(sdj, buffer, strlen(buffer), 0);
           }
         }
